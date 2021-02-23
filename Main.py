6e9c1d4a5f34fdfd19361def651f045274d45f65
Netszky1 <chigotjulien@gmail.com>
2021-02-23 13:20:54 +0100

    last commit

--- Main.py
+++ Main.py
@@ -132,5 +132,5 @@ def Contract(choix):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=5000, debug=True)
